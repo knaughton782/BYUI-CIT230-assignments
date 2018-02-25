24aa8845dc541a454b7bb4c9fa747bd62bd67c75
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-02-24 22:09:17 -0700

    windchill.js started, gallery started

---
+++ assignments/lesson-7/scripts/windchill.js
@@ -0,0 +1,10 @@
+/*The formula to calculate the wind chill factor is:
+
+f = 35.74 + 0.6215 t - 35.75 s0.16 + 0.4275 t s0.16 
+
+where f is the wind chill factor in Fahrenheit, t is the air average temperature in Fahrenheit, and s is the wind speed in miles per hour.  */
+
+function windChill() {
+  var windSpeed = 5;
+  var avgTemp = (90 + 60) / 2;
+}
