c1a0c3b754f68fcaeb69ae0fafba744600e383e2
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-10-27 19:41:51 -0700

    working date, working hamburger

--- assignments/lesson-6/scripts/date.js
+++ assignments/lesson-6/scripts/date.js
@@ -1,5 +1,5 @@
 var footerDate = new Date();
-var day = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+var day = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 var yr;
 
