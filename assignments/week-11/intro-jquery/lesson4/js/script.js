deb0a8017457bb511c446d59c9b0542feca677d7
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-12-07 12:47:48 -0700

    updating jquery lesson

--- assignments/week-11/intro-jquery/lesson4/js/script.js
+++ assignments/week-11/intro-jquery/lesson4/js/script.js
@@ -1,8 +1,52 @@
 window.onload = function () {
+  /**********************************************/
+  $('.dropdownMenu > li').hover(function() {
+    $(this).children('ul').slideDown(0500);
+  }, function() {
+      $(this).children('ul').slideUp(1000);
+  })
+    /**********************************************/
 
+    $('.toggleBtn').click(function(){
+        $(this).toggleClass('toggleOn');
+    })
+  
+    /**********************************************/
+
+    $('.navigation > li').hover(function() {
+
+        $(this).toggleClass('openSesame');
+    //     //executes on mouse hover
+    //     $(this).addClass('openSesame');
+    // }, function (){
+    //     //executes on mouse leave
+    //     $(this).removeClass('openSesame');
+
+    })
+    
+    /**********************************************/
+    
+    // var box1 = $('.box1');
+    // var box2 = $('.box2');
+
+    // function leftToRight() {
+    //     if (box2.html() == '') {
+    //         box2.html(box1.html());
+    //         box1.html('');
+    //     }
+    // }
+
+    // $('#LeftToRight').click(leftToRight);
+    // function rightToLeft() {
+    //     if (box1.html() == '') {
+    //         box1.html(box2.html());
+    //         box2.html('');
+    //     }
+    // }
+
+    // $('#RightToLeft').click(rightToLeft);
    
-   
-   
+  /**********************************************/
     // var nameVal = $('input[name="nameVal"]');
 
     // function fillEmpty() {
@@ -12,27 +56,27 @@ window.onload = function () {
     // }
     // $('#ReqAQuoteBtn').click(fillEmpty);
     
-    
+    /**********************************************/
        
     // $('.coreValues').append("<li>This is a new list item</li>");
     // $('<li>This is the appendto method list item</li>').appendTo(".coreValues");
     // $('.coreValues').prepend('<li>Prepended item</li>');
     // $('<li>This is the prependto method list item</li>').prependTo(".coreValues");
 
+    /**********************************************/
 
+    //     console.log($('input[type="text"]').css('background-color'));
 
-//     console.log($('input[type="text"]').css('background-color'));
+    //    var toggleStyle = function () {
 
-//    var toggleStyle = function () {
+    //         var inputFields = $('input[type="text"]');
+    //         if (inputFields.css('background-color') === 'rgb(255, 0, 0)' ) {
+    //             inputFields.css('background-color', 'rgb(255, 255, 255)');
+    //         } else {
+    //             inputFields.css('background-color', 'rgb(255, 0, 0)');
+    //         }
+    //     }
 
-//         var inputFields = $('input[type="text"]');
-//         if (inputFields.css('background-color') === 'rgb(255, 0, 0)' ) {
-//             inputFields.css('background-color', 'rgb(255, 255, 255)');
-//         } else {
-//             inputFields.css('background-color', 'rgb(255, 0, 0)');
-//         }
-//     }
-
-//     $('#ReqAQuoteBtn').click(toggleStyle);
+    //     $('#ReqAQuoteBtn').click(toggleStyle);
 
 }
