0c9d3c232af1957e506e12d99890dfe02515e996
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-12-07 11:55:08 -0700

    jquery lesson 4

--- assignments/week-11/intro-jquery/lesson4/js/script.js
+++ assignments/week-11/intro-jquery/lesson4/js/script.js
@@ -1,18 +1,38 @@
 window.onload = function () {
 
+   
+   
+   
+    // var nameVal = $('input[name="nameVal"]');
 
-    console.log($('input[type="text"]').css('background-color'));
+    // function fillEmpty() {
+    //     if (nameVal.val() == '') {
+    //         nameVal.val('John Doe');
+    //     }
+    // }
+    // $('#ReqAQuoteBtn').click(fillEmpty);
+    
+    
+       
+    // $('.coreValues').append("<li>This is a new list item</li>");
+    // $('<li>This is the appendto method list item</li>').appendTo(".coreValues");
+    // $('.coreValues').prepend('<li>Prepended item</li>');
+    // $('<li>This is the prependto method list item</li>').prependTo(".coreValues");
 
-   var toggleStyle = function () {
 
-        var inputFields = $('input[type="text"]');
-        if (inputFields.css('background-color') === 'rgb(255, 0, 0)' ) {
-            inputFields.css('background-color', 'rgb(255, 255, 255)');
-        } else {
-            inputFields.css('background-color', 'rgb(255, 0, 0)');
-        }
-    }
 
-    $('#ReqAQuoteBtn').click(toggleStyle);
+//     console.log($('input[type="text"]').css('background-color'));
+
+//    var toggleStyle = function () {
+
+//         var inputFields = $('input[type="text"]');
+//         if (inputFields.css('background-color') === 'rgb(255, 0, 0)' ) {
+//             inputFields.css('background-color', 'rgb(255, 255, 255)');
+//         } else {
+//             inputFields.css('background-color', 'rgb(255, 0, 0)');
+//         }
+//     }
+
+//     $('#ReqAQuoteBtn').click(toggleStyle);
 
 }
