b89acc0979e40180420cec9fdec5b6e6aaa6d3ad
Kirsten Naughton <kirsten.naughton@gmail.com>
2018-12-11 21:46:41 -0700

    added final project planning document link to assignment portal page

--- assignments/week-11/intro-jquery/Lesson5/js/script.js
+++ assignments/week-11/intro-jquery/Lesson5/js/script.js
@@ -1,12 +1,36 @@
-function randomize() {
-    var colors = ['Red', 'Green', 'Yellow', 'Blue', 'Purple']
-    return colors[parseInt(Math.random() * colors.length)];
-}
+window.onload = function () {
 
-function calculateDistance(x, y) {
-    var symbolPositionX = 97;
-    var symbolPositionY = 106;
-    return Math.sqrt(((x - symbolPositionX) * (x - symbolPositionX)) + ((y - symbolPositionY) * (y - symbolPositionY)));
-}
+    $('.boxHolder').on({
+        click: function() {
 
-var statusDisplay = $('.status')
+    },
+        dblclick: function() {
+
+    }
+    })
+
+    
+    // $('.boxHolder').on('click', 'li', function () {
+    //     $(this).addClass('greenBox');
+    // })
+
+    // $('.boxHolder').on('dblclick', 'li', function(){
+    //     $(this).addClass('orangeBox');
+    // })
+
+
+
+    function randomize() {
+        var colors = ['Red', 'Green', 'Yellow', 'Blue', 'Purple']
+        return colors[parseInt(Math.random() * colors.length)];
+    }
+
+    function calculateDistance(x, y) {
+        var symbolPositionX = 97;
+        var symbolPositionY = 106;
+        return Math.sqrt(((x - symbolPositionX) * (x - symbolPositionX)) + ((y - symbolPositionY) * (y - symbolPositionY)));
+    }
+
+    var statusDisplay = $('.status');
+
+}
